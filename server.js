f469412895e61501f869cb9ebd1c1332a1947645
sumeetpevekar <sumeetpevekar1810@gmail.com>
2024-07-09 12:07:04 +0530

    cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const connectDb = require('./utils/db');
 const errorMiddleware = require('./middleware/error-middleware');
 
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : "https://blogadda-website.vercel.app",
     methods : "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials : true,
 }
