27eb01293b4371c735ae2991dafec8964e188b74
sumeet pevekar <100433473+sumeetpevekar@users.noreply.github.com>
2024-07-09 12:41:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require("cors")
-const port = 5000;
-const authRoute = require('./router/auth-router')
-const contactRoute = require('./router/contact-router')
-const adminRoute = require('./router/admin-router')
-const userRoute = require('./router/user-router')
-// const router = require('./router/auth-router')
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "https://blogadda-website.vercel.app",
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+const authRoute = require('./router/auth-router');
+const contactRoute = require('./router/contact-router');
+const adminRoute = require('./router/admin-router');
+const userRoute = require('./router/user-router');
 const connectDb = require('./utils/db');
 const errorMiddleware = require('./middleware/error-middleware');
 
-const corsOptions = {
-    origin : "https://blogadda-website.vercel.app",
-    methods : "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials : true,
-}
-app.use(cors(corsOptions))
-
-app.use(express.json())
 app.use('/api/auth', authRoute);
 app.use('/api/form', contactRoute);
 app.use('/api/user', userRoute);
 app.use('/api/admin', adminRoute);
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 connectDb().then(() => {
-    app.listen(port, () => {
-        console.log(`listening on http://localhost:${port}`)
-    })
+  app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+  });
+}).catch(err => {
+  console.error('Database connection failed', err);
 });
